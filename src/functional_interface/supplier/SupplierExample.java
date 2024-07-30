e903335555bdb9d1d687bc928b38860bdb28399b
rodwanz <wanzervai@gmail.com>
2024-07-30 12:44:01 -0300

    Suportando fluentemente!

---
+++ src/functional_interface/supplier/SupplierExample.java
@@ -0,0 +1,15 @@
+package functional_interface.supplier;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<String> suporte = () -> "Mandando brasa!";
+        List<String> entendendoSuporte = Stream.generate(suporte).limit(5)
+                .collect(Collectors.toList());
+        entendendoSuporte.forEach(s -> System.out.println(s));
+    }
+}
